cee9c0b832f1766e0b8bc6b115205b7013efd71d
JohnnyJamesNavarro <johnnyjamesnavarro@gmail.com>
2020-05-14 09:11:31 -0500

    Added markers to selected locations on the interactive map.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 const MapScreen = () => {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const mapRegion = {
     latitude: 37.77,
     longitude: -122.43,
@@ -10,7 +12,32 @@ const MapScreen = () => {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style={styles.map} region={mapRegion} />;
+  const selectLocationHandler = (event) => {
+    setSelectedLocation({
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
+    });
+  };
+
+  let markerCoordinates;
+
+  if (selectedLocation)
+    markerCoordinates = {
+      latitude: selectedLocation.latitude,
+      longitude: selectedLocation.longitude,
+    };
+
+  return (
+    <MapView
+      style={styles.map}
+      region={mapRegion}
+      onPress={selectLocationHandler}
+    >
+      {markerCoordinates && (
+        <Marker title="Picked Location" coordinate={markerCoordinates}></Marker>
+      )}
+    </MapView>
+  );
 };
 
 MapScreen.navigationOptions = {
